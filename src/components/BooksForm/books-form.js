04847d503693cb86b820060ce8fe804becaacdd3
webmarkyn <web.markyn@gmail.com>
2020-03-31 01:01:17 +0300

    Implemented adding books functionality

--- src/components/BooksForm/books-form.js
+++ src/components/BooksForm/books-form.js
@@ -1,19 +1,52 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { createBook } from '../../actions';
 
 import './books-form.css';
 
 const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
 
-const BooksForm = () => (
-  <div className="books-form">
-    <form>
-      <input type="text" placeholder="Title" />
-      <select name="category" id="category">
-        {categories.map(cat => (<option key={cat} value={cat}>{cat}</option>))}
-      </select>
-      <input type="submit" value="Submit" />
-    </form>
-  </div>
-);
+class BooksForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      category: '',
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-export default BooksForm;
+  handleSubmit(e) {
+    const { title, category } = this.state;
+    const { createBook } = this.props;
+    e.preventDefault();
+    createBook({ id: Math.floor(Math.random() * 10).toString(), title, category });
+  }
+
+  handleChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  render() {
+    const { title, category } = this.state;
+    return (
+      <div className="books-form">
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" placeholder="Title" name="title" value={title} onChange={this.handleChange} />
+          <select name="category" id="category" value={category} onChange={this.handleChange}>
+            {categories.map(cat => (<option key={cat} value={cat}>{cat}</option>))}
+          </select>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
+  }
+}
+
+BooksForm.propTypes = {
+  createBook: PropTypes.func.isRequired,
+};
+
+export default connect(null, { createBook })(BooksForm);
